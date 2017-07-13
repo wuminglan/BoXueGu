48fb7bd8caf070efbbdf0d0f6ed8697a809e35ad
wuminglan <1037279225@qq.com>
2017-07-13 10:56:55 +0800

    再次整理文件夹归类与创建index.php并简化了路径

--- index.php
+++ index.php
@@ -3,10 +3,23 @@ header("content-type:text/html;charset=utf8");
 
  
 // include '/views/dashboard/index.html';
+// include 'http://www.bxg.com/index.php/views/dashboard/index.html';
 // var_dump($_SERVER); 
-
+$path='';
+if(array_key_exists('PATH_INFO',$_SERVER)){
  $path = $_SERVER['PATH_INFO'];
 //   var_dump($path);
- 
-
+    $path=substr($path,1);
+    // http://www.bxg.com/index.php/views/dashboard/index.html
+    $arr=explode('/',$path);
+    if(count($arr)==2){
+        $path='views/'.$arr[0].'/'.$arr[1];
+    }else if(count($arr)==1){
+        $path='views/dashboard/'.$arr[0];
+}
+    }else{
+        // http://www.bxg.com/index.php访问
+        $path='views/dashboard/index';
+    }
+      include $path.'.html';
 ?>
